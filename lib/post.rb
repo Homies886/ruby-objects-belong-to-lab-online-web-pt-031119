930dea3733d3ef0746283c5b8b1a07d29e43418a
Nicholas Rodriguez <niko876@hotmail.com>
2019-04-08 23:57:08 +0000

    Done.

---
+++ lib/post.rb
@@ -0,0 +1,18 @@
+class Post
+  
+  attr_accessor :title, :author
+  
+  def initialize
+    @title = title
+    @author = author
+  end
+  
+  def author
+    @author
+  end
+  
+  def title
+    @title
+  end
+  
+end  
---
+++ lib/song.rb
@@ -0,0 +1,18 @@
+class Song
+  
+  attr_accessor :title, :artist
+  
+  def initialize
+    @title = title
+    @artist = artist
+  end
+  
+  def artist
+    @artist
+  end
+  
+  def title
+    @title
+  end
+  
+end  
